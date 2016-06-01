d35f9be66834007968e0382e032e72079ed65fa2
hassineBM <hsine.benmessaoud@gmail.com>
2016-06-01 07:38:27 +0000

    refactoring

---
+++ public/script.js
@@ -0,0 +1,33 @@
+  $( document ).ready(function() {
+    $('#submitFile').click(function(){
+      var file = $('input[type=file]')[0].files[0];
+      console.log(file)
+      var data = new FormData();
+      data.append("file1", file, "file1")
+      
+      console.log(data)
+      $.ajax({
+        url : "/fileanalyse/",
+        type: "POST",
+        data : data,
+        cache: false,
+		    processData: false,
+		    contentType: false,
+		    async : false,
+        success: function(data, textStatus, jqXHR)
+        {
+          alert(JSON.stringify(data))
+        },
+        error: function (jqXHR, textStatus, errorThrown)
+        { 
+          alert("fuck")
+        }
+      });
+    });
+  });
+  
+  $(document).on('change', ':file', function(e) {
+    var fileName = $('input[type=file]').val().replace(/C:\\fakepath\\/i, '');
+    console.log(fileName);
+    $('#fileName').text(fileName);
+    });
